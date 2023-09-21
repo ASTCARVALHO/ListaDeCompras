15b179c0209bdf36d5fad3441a1a444481f9ee4c
antonio.trajano <ansetrcaastc@gmail.com>
2023-09-21 09:39:56 -0300

    .

--- src/main/java/ufpb/dcx/AntonioSergio/ListaDeCompras/ListaDeCompras.java
+++ src/main/java/ufpb/dcx/AntonioSergio/ListaDeCompras/ListaDeCompras.java
@@ -3,6 +3,10 @@ package ufpb.dcx.AntonioSergio.ListaDeCompras;
 import javax.swing.*;
 
 public class ListaDeCompras {
+
+    //adicionar e remover produtos
+    //cada produto será um objeto com preço tipo e nome
+    //terá uma funcionalidade para prever a quantidade de dinheiro que irá gstar nessas compras
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
