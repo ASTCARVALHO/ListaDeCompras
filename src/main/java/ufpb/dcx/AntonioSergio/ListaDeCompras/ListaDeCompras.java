c6ce2117a94976cc8c50bb81e4b78c4a2b252bd4
antonio.trajano <ansetrcaastc@gmail.com>
2023-09-21 08:52:48 -0300

    .

--- src/main/java/ufpb/dcx/AntonioSergio/ListaDeCompras/ListaDeCompras.java
+++ src/main/java/ufpb/dcx/AntonioSergio/ListaDeCompras/ListaDeCompras.java
@@ -1,2 +1,10 @@
-package ufpb.dcx.AntonioSergio.ListaDeCompras;public class ListaDeCompras {
+package ufpb.dcx.AntonioSergio.ListaDeCompras;
+
+import javax.swing.*;
+
+public class ListaDeCompras {
+    public static void main(String[] args) {
+        JFrame listaDeComprasGUI = new ListaDeComprasGUI();
+
+    }
 }
--- src/main/java/ufpb/dcx/AntonioSergio/ListaDeCompras/ListaDeComprasGUI.java
+++ src/main/java/ufpb/dcx/AntonioSergio/ListaDeCompras/ListaDeComprasGUI.java
@@ -1,2 +1,22 @@
-package ufpb.dcx.AntonioSergio.ListaDeCompras;public class ListaDeComprasGUI {
+package ufpb.dcx.AntonioSergio.ListaDeCompras;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ListaDeComprasGUI extends JFrame {
+    public ListaDeComprasGUI(){
+        setTitle("Listade compras");
+        setSize(400,700);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new GridLayout(3,3));
+        JButton adicionarbtn = new JButton("Adicionar");
+        adicionarbtn.setForeground(new Color(255, 0, 0));
+        add(adicionarbtn);
+        JButton removertbtn = new JButton("Remover");
+        removertbtn.setBackground(new Color(255, 0, 0));
+        getContentPane().add(removertbtn);
+    }
 }
