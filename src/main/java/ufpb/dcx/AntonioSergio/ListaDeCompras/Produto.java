da567973762482d70868089f387aaf9b066c6dbe
ASTCARVALHO <ansetrcaastc@gmail.com>
2023-09-21 14:15:54 -0300

    .

--- src/main/java/ufpb/dcx/AntonioSergio/ListaDeCompras/Produto.java
+++ src/main/java/ufpb/dcx/AntonioSergio/ListaDeCompras/Produto.java
@@ -4,4 +4,40 @@ public class Produto {
     private String nome;
     private double preco;
     private TipoProduto tipoProduto;
+    private int quantidade;
+    public Produto(String nome, double preco, TipoProduto tipoProduto, int quantidade){
+        this.nome = nome;
+        this.preco = preco;
+        this.tipoProduto = tipoProduto;
+        this.quantidade = quantidade;
+    }
+    public Produto(String nome, double preco, int quantidade){
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public TipoProduto getTipoProduto() {
+        return tipoProduto;
+    }
+
+    public void setTipoProduto(TipoProduto tipoProduto) {
+        this.tipoProduto = tipoProduto;
+    }
 }
--- src/main/java/ufpb/dcx/AntonioSergio/ListaDeCompras/SistemaLista.java
+++ src/main/java/ufpb/dcx/AntonioSergio/ListaDeCompras/SistemaLista.java
@@ -1,4 +1,12 @@
 package ufpb.dcx.AntonioSergio.ListaDeCompras;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PropertyResourceBundle;
+
 public class SistemaLista {
+    //cadastra novo produto na lista
+    // remover produto na lista
+    //somar valor total dos produtos da lista
+    List<Produto> produtos = new LinkedList<>();
 }
