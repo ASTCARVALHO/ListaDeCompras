5f136f7ec6139a0675045a8ae1643534fbd105e5
ASTCARVALHO <ansetrcaastc@gmail.com>
2023-09-25 16:21:05 -0300

    incompleto

--- src/main/java/ufpb/dcx/AntonioSergio/ListaDeCompras/ListaDecomprasInterface.java
+++ src/main/java/ufpb/dcx/AntonioSergio/ListaDeCompras/ListaDecomprasInterface.java
@@ -4,5 +4,6 @@ public interface ListaDecomprasInterface {
     public void adicionarProduto(String nome, double preco,String tipoProduto, int quantidade);
     public  void removerProduto(String nome);
     public double somaVaoloresTotal();
+    public void limparLista();
 
 }
