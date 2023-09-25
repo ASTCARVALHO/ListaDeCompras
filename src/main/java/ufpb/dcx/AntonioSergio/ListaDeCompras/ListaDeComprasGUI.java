a41d2a86c6f48276b869d98676f9708633bda6fb
ASTCARVALHO <ansetrcaastc@gmail.com>
2023-09-25 15:39:54 -0300

    incompleto

--- src/main/java/ufpb/dcx/AntonioSergio/ListaDeCompras/ListaDeComprasGUI.java
+++ src/main/java/ufpb/dcx/AntonioSergio/ListaDeCompras/ListaDeComprasGUI.java
@@ -5,34 +5,50 @@ import java.awt.event.ActionEvent;
 
 public class ListaDeComprasGUI extends JFrame {
     SistemaLista sistema = new SistemaLista();
-
     DefaultListModel<String> listaModel;
     JLabel valorTotal;
+    JList<String> lista;
+    JButton adicionarbtn,removerbtn,limpaListabtn;
     public ListaDeComprasGUI(){
         setTitle("Listade compras");
-        setSize(381,729);
+        setSize(360,800);
         setLocationRelativeTo(null);
         setResizable(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new GridLayout(4,0));
+        setLayout(null);
+
+
         listaModel = new DefaultListModel<>();
-        JList<String> lista = new JList<>(listaModel);
+        lista = new JList<>(listaModel);
+        lista.setBounds(0,0,360,484);
         add(lista);
+
         valorTotal = new JLabel();
-        valorTotal.setBackground(new Color(0x0000FF));
+        valorTotal.setBounds(0,506,145,46);
+        valorTotal.setForeground(new Color(241, 5, 139));
+        valorTotal.setBackground(new Color(0, 100, 255,134));
         add(valorTotal);
-        JButton adicionarbtn = new JButton();
+
+        adicionarbtn = new JButton();
+        adicionarbtn.setBounds(85,603,189,59);
         adicionarbtn.setText("Adicionar");
         adicionarbtn.setForeground(new Color(255, 0, 0));
         add(adicionarbtn);
-        JButton removertbtn = new JButton("Remover");
-        removertbtn.setBackground(new Color(255, 0, 0));
-        removertbtn.setSize(600,100);
-        getContentPane().add(removertbtn);
+
+        removerbtn = new JButton("Remover");
+        removerbtn.setBounds(85,680,189,59);
+        removerbtn.setBackground(new Color(255, 0, 0));
+        add(removerbtn);
+
+        limpaListabtn = new JButton("clear");
+        limpaListabtn.setBounds(295,509,52,43);
+        limpaListabtn.setBackground(new Color(227, 255, 8, 255));
+        add(limpaListabtn);
+
         setVisible(true);
-        atualizar();
         adicionarbtn.addActionListener(this::adicionar);
-        removertbtn.addActionListener(this::remover);
+        removerbtn.addActionListener(this::remover);
+        atualizar();
     }
 
     private void adicionar(ActionEvent actionEvent){
@@ -42,9 +58,6 @@ public class ListaDeComprasGUI extends JFrame {
         double preco = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor previsto"));
         int quantidade = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade prevista"));
         sistema.adicionarProduto(nome,preco,tipo,quantidade);
-        double valorTotalCalc = sistema.somaVaoloresTotal();
-        valorTotal.setText(Double.toString(valorTotalCalc));
-        listaModel.removeAllElements();
         atualizar();
         }
     }
@@ -54,17 +67,17 @@ public class ListaDeComprasGUI extends JFrame {
         }
         String nome = JOptionPane.showInputDialog("Digite o nome");
         sistema.removerProduto(nome);
-        listaModel.removeAllElements();
-        double valorTotalCalc = sistema.somaVaoloresTotal();
-        valorTotal.setText(Double.toString(valorTotalCalc));
         atualizar();
-
     }
     public void atualizar(){
+        String text = "Valor Total: ";
+        listaModel.removeAllElements();
         for (Produto p: sistema.produtos) {
             listaModel.addElement(p.toString());
         }
         sistema.salvarDados();
+        double valorTotalCalc = sistema.somaVaoloresTotal();
+        valorTotal.setText(text + Double.toString(valorTotalCalc) + " R$");
     }
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
