f6a0c1d3701711cca9edaab9a7fe6ed958cbdfbf
antonio.trajano <ansetrcaastc@gmail.com>
2023-09-26 11:14:39 -0300

    .

--- src/main/java/ufpb/dcx/AntonioSergio/ListaDeCompras/ListaDecomprasInterface.java
+++ src/main/java/ufpb/dcx/AntonioSergio/ListaDeCompras/ListaDecomprasInterface.java
@@ -1,6 +1,13 @@
 package ufpb.dcx.AntonioSergio.ListaDeCompras;
 
 public interface ListaDecomprasInterface {
+    /**
+     *
+     * @param nome
+     * @param preco
+     * @param tipoProduto
+     * @param quantidade
+     */
     public void adicionarProduto(String nome, double preco,String tipoProduto, int quantidade);
     public  void removerProduto(String nome);
     public double somaVaoloresTotal();
